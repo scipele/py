ad3a3926e7d6f6d73e1710167cf0d2a720227d13
Tony Sciple <scipele@yahoo.com>
2024-12-01 19:17:42 -0600

    minot update

--- loops/howToLoop.py
+++ loops/howToLoop.py
@@ -9,14 +9,16 @@ def ex_for_each(fruits):
 def ex_loop_with_index(fruits):
     print('This is an example for each loop with an enumerated index:')
     for indx, fruit in enumerate(fruits):
-        print('Index:', indx, 'fruit type:', fruit)
+        print('    Index:', indx, 'fruit type:', fruit)
+    print('\n')
 
 def ex_while():
-    print('This is an example for each loop:')
+    print('This is an example while loop:')
     count = 0
     while count < 5:
-        print(count)
+        print('    ', count, end="") # <-- You can use and end="" the avoid that newline character
         count += 1
+    print('\n')
 
 if __name__ == "__main__":
     fruits = ['apple', 'orange', 'pear', 'bananna']
